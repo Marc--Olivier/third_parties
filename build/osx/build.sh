596810b747ea7ecee32f263dfb39f712a79c9ced
Marc-Olivier Andrez <mo.andrez@gmail.com>
2017-05-10 07:54:25 +0200

    Compile boost for macOS

--- build/osx/build.sh
+++ build/osx/build.sh
@@ -7,6 +7,15 @@ SRC_DIR=$ROOT_DIR/src
 mkdir -p $INSTALL_DIR
 mkdir -p $BUILD_AUX_DIR
 
+# boost
+# http://www.boost.org/doc/libs/1_64_0/more/getting_started/unix-variants.html
+cd $SRC_DIR/boost
+# mkdir -p $BUILD_AUX_DIR/boost_bootstrap
+# ./bootstrap.sh --prefix=$BUILD_AUX_DIR/boost_bootstrap 
+./bootstrap.sh
+./b2 headers # https://marc.info/?l=boost-users&m=141745580221500&w=2
+./b2 link=shared cxxflags=-std=c++1z --build-type=minimal --build-dir=$BUILD_AUX_DIR/boost --prefix=$INSTALL_DIR install headers
+
 cd $BUILD_AUX_DIR
 cmake -G "CodeBlocks - Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_CXX_FLAGS="-std=c++1z" $SRC_DIR
 ninja
